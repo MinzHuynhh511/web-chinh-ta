d0c5f3eb088e18f04ceef00dc8c83e1f72f7a110
MinzHuynhh511 <minh05112k5@gmail.com>
2026-04-16 23:44:31 +0700

    Add luyentap quiz to allQuizzes array

--- src/data/index.js
+++ src/data/index.js
@@ -2,12 +2,14 @@
 // Để thêm bộ đề mới: tạo file .json trong thư mục này và import vào đây
 
 import kiemtrachinhta from './tieng-viet-chinh-ta.json';
+import luyentap from './luyen-tap.json';
 
 
 // Mảng này tự động được đọc để hiển thị trang chủ
 // Thứ tự trong mảng = thứ tự hiển thị trên trang chủ
 const allQuizzes = [
-  kiemtrachinhta
+  kiemtrachinhta,
+  luyentap
 ];
 
 export default allQuizzes;
