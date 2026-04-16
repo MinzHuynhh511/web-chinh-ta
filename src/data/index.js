ebada5d250ada9dc644df43ca0a855f44b806511
MinzHuynhh511 <minh05112k5@gmail.com>
2026-04-16 23:45:38 +0700

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,14 +1,12 @@
 // Auto-import tất cả bộ đề thi
 // Để thêm bộ đề mới: tạo file .json trong thư mục này và import vào đây
 
-import kiemtrachinhta from './tieng-viet-chinh-ta.json';
 import luyentap from './luyen-tap.json';
 
 
 // Mảng này tự động được đọc để hiển thị trang chủ
 // Thứ tự trong mảng = thứ tự hiển thị trên trang chủ
 const allQuizzes = [
-  kiemtrachinhta,
   luyentap
 ];
 
