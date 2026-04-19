a5c2515c94b5344fc7c33236a634244d0c761fa6
MinzHuynhh511 <minh05112k5@gmail.com>
2026-04-19 22:07:48 +0700

    Add new quiz 'tracnghiemtoeic' to allQuizzes

--- src/data/index.js
+++ src/data/index.js
@@ -2,12 +2,13 @@
 // Để thêm bộ đề mới: tạo file .json trong thư mục này và import vào đây
 
 import luyentap from './luyen-tap.json';
-
+import tracnghiemtoeic from './TRẮC NGHIỆM TA.json';
 
 // Mảng này tự động được đọc để hiển thị trang chủ
 // Thứ tự trong mảng = thứ tự hiển thị trên trang chủ
 const allQuizzes = [
-  luyentap
+  luyentap,
+  tracnghiemtoeic
 ];
 
 export default allQuizzes;
