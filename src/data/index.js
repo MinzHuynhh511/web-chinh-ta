1b298f038b53bbc234eb043b460b9f0d0343f6d1
MinzHuynhh511 <minh05112k5@gmail.com>
2026-06-05 14:10:58 +0700

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,7 @@
 import luyentap from './luyen-tap.json';
 import tracnghiemtoeicp1 from './TA_309_cau_p1.json';
 import tracnghiemtoeicp2 from './TA_309_cau_p2.json';
-import CNPMPCuoiKy from './CNPMP_Cuoi_Ky.json';
+import cnpm_ck from './CNPMP_Cuoi_Ky.json';
 
 // Mảng này tự động được đọc để hiển thị trang chủ
 // Thứ tự trong mảng = thứ tự hiển thị trên trang chủ
@@ -12,7 +12,7 @@ const allQuizzes = [
   luyentap,
   tracnghiemtoeicp1,
   tracnghiemtoeicp2,
-  CNPMPCuoiKy
+  cnpm_cuoiky
 ];
 
 export default allQuizzes;
