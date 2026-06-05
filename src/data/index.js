16ae7f40d43a0cc9bddc221e4c9ee332a0e1d2b5
MinzHuynhh511 <minh05112k5@gmail.com>
2026-06-05 16:00:42 +0700

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,18 +1,17 @@
 // Auto-import tất cả bộ đề thi
 // Để thêm bộ đề mới: tạo file .json trong thư mục này và import vào đây
 
-import luyentap from './luyen-tap.json';
+import cnpmck from './CNPMP_Cuoi_Ky';
 import tracnghiemtoeicp1 from './TA_309_cau_p1.json';
 import tracnghiemtoeicp2 from './TA_309_cau_p2.json';
-import cnpm from './CNPMP_Cuoi_Ky.json';
+
 
 // Mảng này tự động được đọc để hiển thị trang chủ
 // Thứ tự trong mảng = thứ tự hiển thị trên trang chủ
 const allQuizzes = [
-  luyentap,
+  cnpmck,
   tracnghiemtoeicp1,
-  tracnghiemtoeicp2,
-  cnpm_cuoiky
+  tracnghiemtoeicp2
 ];
 
 export default allQuizzes;
