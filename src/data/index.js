c351d12a95f943d846e866525f0d7ea8a6a08f35
MinzHuynhh511 <minh05112k5@gmail.com>
2026-06-05 14:12:07 +0700

    Rename import variable for CNPMP_Cuoi_Ky.json

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,7 @@
 import luyentap from './luyen-tap.json';
 import tracnghiemtoeicp1 from './TA_309_cau_p1.json';
 import tracnghiemtoeicp2 from './TA_309_cau_p2.json';
-import cnpm_ck from './CNPMP_Cuoi_Ky.json';
+import cnpm from './CNPMP_Cuoi_Ky.json';
 
 // Mảng này tự động được đọc để hiển thị trang chủ
 // Thứ tự trong mảng = thứ tự hiển thị trên trang chủ
